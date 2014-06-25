d46adfd4f9aaf9d40409b62a5387affa4cd37570
Jason Paryani <github@jparyani.com>
2014-06-25 13:54:47 -0700

    Move setup for grain to bash script

---
+++ run_grain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp -r /etc/service /tmp
+test -d /var/log || cp -r /var_original/log /var
+test -d /var/lib || cp -r /var_original/lib /var
+test -d /var/run || cp -r /var_original/run /var
+test -f /var/lock || ln -s /var/run/lock /var/lock
+test -f /var/cache || cp -r /opt/app/cache /var
+test -f /var/feed-icons || cp -r /opt/app/feed-icons /var
+
+/sbin/my_init
