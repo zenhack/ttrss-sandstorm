d48a3b51c0bf05c70779958583ccf53693038924
Jason Paryani <github@jparyani.com>
2014-06-25 13:58:23 -0700

    Take a docker container, exports it, and set it up for sandstorm

--- export_docker.sh
+++ export_docker.sh
@@ -9,14 +9,15 @@ rm -rf etc/service/*/supervise
 
 cd var_original
 rm lock run
+rm -f ../run/crond.reboot
 cp -r ../run .
 ln -s run/lock lock
 cd ..
 
 cd opt
 rm -rf app/.git
-mv app app_original
-ln -s /var/app app
+cp app/sandstorm/bin/* ../bin
+cp app/config.php-sandstorm app/config.php
 cd ../..
 
 cp my_init dockerenv/sbin/
