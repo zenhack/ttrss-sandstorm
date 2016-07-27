47362c09f2a4d412cc871dee6140efa8a88a5302
Jacob Weisz <ocdtrekkie@gmail.com>
2016-07-26 20:12:33 -0500

    Remove F-Droid TTRSS app, add Windows UWP app
    Nils' TTRSS app still doesn't work, even with the latest GitHub release.
    We should remove it until it does work with Sandstorm, and maybe I'll
    file an issue against it and offer some demo credentials for them to
    use.

--- classes/pref/mobile.php
+++ classes/pref/mobile.php
@@ -1,12 +1,17 @@
 <?php
 class Pref_Mobile extends Handler_Protected {
     function index() {
-      print "<h2>Android App Setup</h2>";
-      print "<p>You can easily use this app on your Android device. Just follow these instructions:</p>";
+      print "<h2>Mobile App Setup</h2>";
+	  print "<h2>Android</h2>";
       print "<ul>";
-      print "<li>Download an app.  The Tiny Tiny RSS authors maintain an <a href=\"https://play.google.com/store/apps/details?id=org.fox.ttrss\">app on Google Play</a>.  An app by a different author is available on <a href=\"https://f-droid.org/repository/browse/?fdid=org.ttrssreader\">FDroid</a>.</li>";
       print "<li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>";
-      print "<li>Open the app.  Set the host to be the part of the webkey before the #.  Set the HTTP Auth user to an arbitrary string and the HTTP Auth password to the part of the webkey after the #.</li>";
+      print "<li>You should now be good to go!</li>";
+      print "</ul>";
+	  print "<ul>";
+	  print "<li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>";
+	  print "<li>Open the app.  Set the Server to be the part of the webkey before the #.  You may need to add /api to the end of the server URL.</li>";
+	  print "<li>Set the Username to \"sandstorm\" and the Password to the part of the webkey after the #.</li>";
+	  print "<li>Check the Http Authentication checkbox.</li>";
       print "<li>You should now be good to go!</li>";
       print "</ul>";
   }
