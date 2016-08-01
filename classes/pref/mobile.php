b014af5f6a672ca17c73bf6197e8c7a122934559
Jason Paryani <github@jparyani.com>
2016-08-01 13:43:38 -0700

    Add offer templates back in to mobile.php

--- classes/pref/mobile.php
+++ classes/pref/mobile.php
@@ -1,24 +1,30 @@
 <?php
 class Pref_Mobile extends Handler_Protected {
     function index() {
-      print "<h2>Mobile App Setup</h2>";
-      print "<p>You can easily use this app on your mobile device. Just follow the instructions for your device:</p>";
-	  print "<h2>Android</h2>";
-      print "<ul>";
-      print "<li>Download the official Tiny Tiny RSS app on <a href=\"https://play.google.com/store/apps/details?id=org.fox.ttrss\" target=\"_blank\">Google Play</a>.</li>";
-      print "<li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>";
-      print "<li>Open the app.  Set the host to be the part of the webkey before the #.  Set the HTTP Authentication Login to \"sandstorm\" and the HTTP Authentication Password to the part of the webkey after the #.</li>";
-      print "<li>You should now be good to go!</li>";
-      print "</ul>";
-	  print "<h2>Windows</h2>";
-	  print "<ul>";
-	  print "<li>Download the Tiny Tiny RSS Reader app found on the <a href=\"https://www.microsoft.com/store/apps/9wzdncrdmbn3\" target=\"_blank\">Windows Store</a>. This app has been tested to work with Sandstorm on Windows 10 mobile and desktop. Note: The Windows 8.1 version does not work with Sandstorm.</li>";
-	  print "<li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>";
-	  print "<li>Open the app.  Set the Server to be the part of the webkey before the #.  You may need to add /api to the end of the server URL.</li>";
-	  print "<li>Set the Username to \"sandstorm\" and the Password to the part of the webkey after the #.</li>";
-	  print "<li>Check the Http Authentication checkbox.</li>";
-      print "<li>You should now be good to go!</li>";
-      print "</ul>";
+    print <<<EOD
+<h2>Mobile App Setup</h2>
+<p>You can easily use this app on your mobile device. Just follow the instructions for your device:</p>
+<h2>Android</h2>
+<ul>
+  <li>Download the official Tiny Tiny RSS app on <a href="https://play.google.com/store/apps/details?id=org.fox.ttrss" target="_blank">Google Play</a>.</li>
+  <li>Open the app.  Set the host to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>Set the HTTP Authentication Login to: <div>sandstorm</div> and the HTTP Authentication Password to <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>.</li>
+  <li>You should now be good to go!</li>
+</ul>
+<h2>Windows</h2>
+<ul>
+  <li>Download the Tiny Tiny RSS Reader app found on the <a href="https://www.microsoft.com/store/apps/9wzdncrdmbn3" target="_blank">Windows Store</a>. This app has been tested to work with Sandstorm on Windows 10 mobile and desktop. Note: The Windows 8.1 version does not work with Sandstorm.</li>
+  <li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>
+  <li>
+    Open the app.  Set the Server to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>You may need to add /api to the end of the server URL. <br />
+    Set the Username to: <div>sandstorm</div>
+    And the Password to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>
+  </li>
+  <li>Check the Http Authentication checkbox.</li>
+  <li>You should now be good to go!</li>
+</ul>
+
+<img src="/images/pub_set.png" onload='var script = document.createElement("script");script.type = "text/javascript"; script.src   = "/js/sandstorm-offer-template.js"; document.body.appendChild(script);'></img>
+EOD;
   }
 }
 ?>
--- js/sandstorm-offer-template.js
+++ js/sandstorm-offer-template.js
@@ -1,17 +1,36 @@
+function trySetElement(selector, data) {
+  var nodes = document.querySelectorAll(selector);
+  if (nodes.length === 0) {
+    // Page not rendered yet, try again later
+
+    window.setTimeout(function () {
+      trySetElement(selector, data)
+    }, 100);
+    return;
+  }
+  for (var i = 0; i < nodes.length; ++i) {
+    nodes[i].setAttribute("src", data);
+  };
+}
 
 var messageListener = function(event) {
-  if (event.data.rpcId === "0") {
+  if (event.data.rpcId === "HOST_TEMPLATE") {
     if (event.data.error) {
       console.log("ERROR: " + event.data.error);
     } else {
-      window.setTimeout(function () {
-        var el = document.getElementById("offer-template");
-        el.setAttribute("src", event.data.uri);
-      }, 1000);
+      trySetElement(".offer-host", event.data.uri);
+    }
+  } else if (event.data.rpcId === "TOKEN_TEMPLATE") {
+    if (event.data.error) {
+      console.log("ERROR: " + event.data.error);
+    } else {
+      trySetElement(".offer-token", event.data.uri);
     }
   }
 };
 
 window.addEventListener("message", messageListener);
-var template = "Copy the following webkey URL: " + window.location.protocol + "//" + "$API_HOST#$API_TOKEN";
-window.parent.postMessage({renderTemplate: {rpcId: "0", template: template}}, "*");
+var hostTemplate = window.location.protocol + "//" + "$API_HOST";
+var tokenTemplate = "$API_TOKEN";
+window.parent.postMessage({renderTemplate: {rpcId: "HOST_TEMPLATE", template: hostTemplate, clipboardButton: 'left'}}, "*");
+window.parent.postMessage({renderTemplate: {rpcId: "TOKEN_TEMPLATE", template: tokenTemplate, clipboardButton: 'left'}}, "*");
