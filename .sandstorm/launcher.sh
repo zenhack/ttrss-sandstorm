247d5200bfab49aabe4e908756fd16771c3430fc
Ian Denhardt <ian@zenhack.net>
2020-05-23 12:57:04 -0400

    Fresh porting attempt.
    Some stuff pulled over from Jason's work, but I've done a fresh pass to
    avoid things like checked-in mysql databases (ew).
    
    still TODO:
    
    - database migrations.
    - actually get the network stuff working.

---
+++ .sandstorm/launcher.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Create a bunch of folders under the clean /var that php, nginx, and mysql expect to exist
+mkdir -p /var/lib/mysql
+mkdir -p /var/lib/mysql-files
+mkdir -p /var/lib/nginx
+mkdir -p /var/lib/php/sessions
+mkdir -p /var/log
+mkdir -p /var/log/mysql
+mkdir -p /var/log/nginx
+
+# specific to tt-rss:
+mkdir -p /var/cache/images
+mkdir -p /var/cache/upload
+mkdir -p /var/cache/export
+mkdir -p /var/lock
+
+if [ ! -d /var/feed-icons ]; then
+    cp -r /opt/app/feed-icons /var/
+fi
+
+# Wipe /var/run, since pidfiles and socket files from previous launches should go away
+# TODO someday: I'd prefer a tmpfs for these.
+rm -rf /var/run
+mkdir -p /var/run/php
+rm -rf /var/tmp
+mkdir -p /var/tmp
+mkdir -p /var/run/mysqld
+
+# Ensure mysql tables created
+# HOME=/etc/mysql /usr/bin/mysql_install_db
+HOME=/etc/mysql /usr/sbin/mysqld --initialize
+
+# Spawn mysqld, php
+HOME=/etc/mysql /usr/sbin/mysqld --skip-grant-tables &
+/usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf &
+# Wait until mysql and php have bound their sockets, indicating readiness
+while [ ! -e /var/run/mysqld/mysqld.sock ] ; do
+    echo "waiting for mysql to be available at /var/run/mysqld/mysqld.sock"
+    sleep .2
+done
+while [ ! -e /var/run/php/php7.0-fpm.sock ] ; do
+    echo "waiting for php-fpm7.0 to be available at /var/run/php/php7.0-fpm.sock"
+    sleep .2
+done
+if [ ! -e /var/.db-created ]; then
+    mysql --user root -e 'CREATE DATABASE app'
+    mysql --user root --database app < /opt/app/schema/ttrss_schema_mysql.sql
+    touch /var/.db-created
+fi
+
+# Start nginx.
+/usr/sbin/nginx -c /opt/app/.sandstorm/service-config/nginx.conf -g "daemon off;"
+
+# vim: set ts=4 sw=4 et :
---
+++ .sandstorm/setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo -e "deb http://repo.mysql.com/apt/debian/ stretch mysql-5.7\ndeb-src http://repo.mysql.com/apt/debian/ stretch mysql-5.7" > /etc/apt/sources.list.d/mysql.list
+wget -O /tmp/RPM-GPG-KEY-mysql https://repo.mysql.com/RPM-GPG-KEY-mysql
+apt-key add /tmp/RPM-GPG-KEY-mysql
+
+apt-get update
+apt-get install -y \
+	git \
+	mysql-server \
+	nginx \
+	php7.0-cli \
+	php7.0-curl \
+	php7.0-dev \
+	php7.0-fpm \
+	php7.0-intl \
+	php7.0-mbstring \
+	php7.0-mysql
+service nginx stop
+service php7.0-fpm stop
+service mysql stop
+systemctl disable nginx
+systemctl disable php7.0-fpm
+systemctl disable mysql
+# patch /etc/php/7.0/fpm/pool.d/www.conf to not change uid/gid to www-data
+sed --in-place='' \
+        --expression='s/^listen.owner = www-data/;listen.owner = www-data/' \
+        --expression='s/^listen.group = www-data/;listen.group = www-data/' \
+        --expression='s/^user = www-data/;user = www-data/' \
+        --expression='s/^group = www-data/;group = www-data/' \
+        /etc/php/7.0/fpm/pool.d/www.conf
+# patch /etc/php/7.0/fpm/php-fpm.conf to not have a pidfile
+sed --in-place='' \
+        --expression='s/^pid =/;pid =/' \
+        /etc/php/7.0/fpm/php-fpm.conf
+# patch /etc/php/7.0/fpm/php-fpm.conf to place the sock file in /var
+sed --in-place='' \
+       --expression='s/^listen = \/run\/php\/php7.0-fpm.sock/listen = \/var\/run\/php\/php7.0-fpm.sock/' \
+        /etc/php/7.0/fpm/pool.d/www.conf
+# patch /etc/php/7.0/fpm/pool.d/www.conf to no clear environment variables
+# so we can pass in SANDSTORM=1 to apps
+sed --in-place='' \
+        --expression='s/^;clear_env = no/clear_env=no/' \
+        /etc/php/7.0/fpm/pool.d/www.conf
+# patch mysql conf to not change uid, and to use /var/tmp over /tmp
+# for secure-file-priv see https://github.com/sandstorm-io/vagrant-spk/issues/195
+sed --in-place='' \
+        --expression='s/^user\t\t= mysql/#user\t\t= mysql/' \
+        --expression='s,^tmpdir\t\t= /tmp,tmpdir\t\t= /var/tmp,' \
+        --expression='/\[mysqld]/ a\ secure-file-priv = ""\' \
+        /etc/mysql/my.cnf
+# patch mysql conf to use smaller transaction logs to save disk space
+cat <<EOF > /etc/mysql/conf.d/sandstorm.cnf
+[mysqld]
+# Set the transaction log file to the minimum allowed size to save disk space.
+innodb_log_file_size = 1048576
+# Set the main data file to grow by 1MB at a time, rather than 8MB at a time.
+innodb_autoextend_increment = 1
+EOF
