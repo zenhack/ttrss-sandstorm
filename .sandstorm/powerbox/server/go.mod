ac4a4ae2a303b87340aea102006ab3ec9cfc40ee
Ian Denhardt <ian@zenhack.net>
2020-06-24 22:14:24 -0400

    Misc. work on the powerbox server.

--- .sandstorm/powerbox/server/go.mod
+++ .sandstorm/powerbox/server/go.mod
@@ -3,6 +3,7 @@ module zenhack.net/go/ttrss-powerbox/server
 go 1.14
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/websocket v1.4.2
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2
 )
--- .sandstorm/powerbox/server/main.go
+++ .sandstorm/powerbox/server/main.go
@@ -1,33 +1,101 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/websocket"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const webSocketListenAddr = ":3000"
 
-var proxyListenAddr = ":" + os.Getenv("POWERBOX_PROXY_PORT")
+var (
+	proxyListenAddr = ":" + os.Getenv("POWERBOX_PROXY_PORT")
 
-func main() {
-	go func() {
-		panic(http.ListenAndServe(webSocketListenAddr, newWebSocketServer()))
-	}()
-	panic(http.ListenAndServe(proxyListenAddr, newProxyServer()))
+	mysqlUser = os.Getenv("MYSQL_USER")
+	mysqlDb   = os.Getenv("MYSQL_DATABASE")
+	mysqlUri  = mysqlUser + "@/" + mysqlDb
+)
+
+func chkfatal(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
-func newProxyServer() http.Handler {
+func main() {
+	db, err := sql.Open("mysql", mysqlUri)
+	chkfatal(err)
+	storage, err := NewStorage(db)
+	chkfatal(err)
+	srv := NewServer(storage)
+
+	go func() {
+		panic(http.ListenAndServe(webSocketListenAddr, srv.newWebSocketServer()))
+	}()
+	panic(http.ListenAndServe(proxyListenAddr, srv.newProxyServer()))
+}
+
+func NewServer(storage Storage) Server {
+	srv := Server{
+		storage: storage,
+		mu:      make(chan struct{}, 1),
+	}
+	srv.mu <- struct{}{}
+	return srv
+}
+
+type Server struct {
+	storage Storage
+
+	// Protects fields below
+	mu       chan struct{}
+	wsConn   *websocket.Conn
+	cancelWs context.CancelFunc
+}
+
+func (s Server) newProxyServer() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		log.Println("Got proxy request:", req)
-		w.WriteHeader(401)
-		w.Write([]byte("TODO"))
+		url := req.URL.String()
+		client, err := s.getClientFor(url)
+		if err != nil {
+			log.Printf("Failed to get client for %q: %v", url, err)
+			w.WriteHeader(500)
+			return
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Printf("Error making proxied request: %v", err)
+			w.WriteHeader(500)
+			return
+		}
+		defer resp.Body.Close()
+		wh := w.Header()
+		for k, v := range resp.Header {
+			wh[k] = v
+		}
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
 	})
 }
 
-func newWebSocketServer() http.Handler {
+func (s Server) getClientFor(url string) (*http.Client, error) {
+	token, err := s.storage.GetTokenFor(url)
+	if err != nil {
+		return nil, err
+	}
+	_ = token
+	return nil, errors.New("TODO")
+}
+
+func (s Server) newWebSocketServer() http.Handler {
 	up := &websocket.Upgrader{}
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		conn, err := up.Upgrade(w, req, nil)
@@ -35,14 +103,27 @@ func newWebSocketServer() http.Handler {
 			log.Println(err)
 			return
 		}
-		err = conn.WriteMessage(
-			websocket.TextMessage,
-			[]byte(`{"message": "Look the websocket works!"}`),
-		)
-		if err != nil {
-			log.Printf("Error writing to websocket: %q", err)
-		}
-		conn.Close()
-
+		ctx, cancel := context.WithCancel(req.Context())
+		s.newConn(conn, cancel)
+		<-ctx.Done()
 	})
+}
+
+func (s Server) newConn(conn *websocket.Conn, cancel context.CancelFunc) {
+	s.withMu(func() {
+		if s.wsConn != nil {
+			s.wsConn.Close()
+			s.cancelWs()
+		}
+		s.wsConn = conn
+		s.cancelWs = cancel
+	})
+}
+
+func (s Server) withMu(fn func()) {
+	<-s.mu
+	defer func() {
+		s.mu <- struct{}{}
+	}()
+	fn()
 }
