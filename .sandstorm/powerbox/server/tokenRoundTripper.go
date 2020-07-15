a16dc7ae5380f51c70d9e4c5f16c29e2bad9cd43
Ian Denhardt <ian@zenhack.net>
2020-07-14 20:49:11 -0400

    Avoid duplicating query strings.

--- .sandstorm/powerbox/server/tokenRoundTripper.go
+++ .sandstorm/powerbox/server/tokenRoundTripper.go
@@ -12,15 +12,15 @@ type tokenRoundTripper struct {
 func (tr tokenRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "Bearer "+tr.token)
 
-	// The token will have included the path info that was submitted with
-	// the corresponding powerbox request. If we also include a path here,
+	// The token will have included the path info & query string that was submitted
+	// with the corresponding powerbox request. If we also that info here,
 	// we'll duplicate it. E.g. if we make a pb request for http://example.com/foo,
 	// and then use that token to make an http request for http://example.com/foo,
 	// we'll actually end up sending a request for http://example.com/foo/foo.
-	//
-	// So we leave off the path.
 	req.URL.Path = ""
 	req.URL.RawPath = ""
+	req.URL.ForceQuery = false
+	req.URL.RawQuery = ""
 
 	return tr.underlying.RoundTrip(req)
 }
