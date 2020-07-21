b5f844b27536ac3d54fcf7f18a32356476cc3d54
Ian Denhardt <ian@zenhack.net>
2020-07-21 14:05:34 -0400

    Remove instructions about windows client.
    As @ocdtrekkie points out, the app is no longer available.

--- classes/pref/mobile.php
+++ classes/pref/mobile.php
@@ -10,18 +10,6 @@ class Pref_Mobile extends Handler_Protected {
   <li>Open the app.  Set the host to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>Set the HTTP Authentication Login to: <div>sandstorm</div> and the HTTP Authentication Password to <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>.</li>
   <li>You should now be good to go!</li>
 </ul>
-<h2>Windows</h2>
-<ul>
-  <li>Download the Tiny Tiny RSS Reader app found on the <a href="https://www.microsoft.com/store/apps/9wzdncrdmbn3" target="_blank">Windows Store</a>. This app has been tested to work with Sandstorm on Windows 10 mobile and desktop. Note: The Windows 8.1 version does not work with Sandstorm.</li>
-  <li>In the header bar of this site, you should see a key icon (you may need to open the menu). Click the key icon and create a new key. Save the webkey URL it gives you.</li>
-  <li>
-    Open the app.  Set the Server to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>You may need to add /api to the end of the server URL. <br />
-    Set the Username to: <div>sandstorm</div>
-    And the Password to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>
-  </li>
-  <li>Check the Http Authentication checkbox.</li>
-  <li>You should now be good to go!</li>
-</ul>
 
 <img src="/images/logo_small.png" onload='var script = document.createElement("script");script.type = "text/javascript"; script.src   = "/js/sandstorm-offer-template.js"; document.body.appendChild(script);'></img>
 EOD;
