5b9b965e5a79304ec6f3ac282183fe40e7129655
Ian Denhardt <ian@zenhack.net>
2020-07-21 20:14:41 -0400

    Make offer iframe backgrounds white.
    Solves the visibility issue mentioned in #9

--- classes/pref/mobile.php
+++ classes/pref/mobile.php
@@ -7,7 +7,7 @@ class Pref_Mobile extends Handler_Protected {
 <h2>Android</h2>
 <ul>
   <li>Download the official Tiny Tiny RSS app on <a href="https://play.google.com/store/apps/details?id=org.fox.ttrss" target="_blank">Google Play</a>.</li>
-  <li>Open the app.  Set the host to: <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>Set the HTTP Authentication Login to: <div>sandstorm</div> and the HTTP Authentication Password to <iframe style="width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>.</li>
+  <li>Open the app.  Set the host to: <iframe style="background-color: white; color: black; width: 100%; height: 16px; margin: 0; border: 0;" class="offer-host"></iframe>Set the HTTP Authentication Login to: <div>sandstorm</div> and the HTTP Authentication Password to <iframe style="background-color: white; color: black; width: 100%; height: 16px; margin: 0; border: 0;" class="offer-token"></iframe>.</li>
   <li>You should now be good to go!</li>
 </ul>
 
