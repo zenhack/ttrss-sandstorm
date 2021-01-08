c236b58386c12ee2f81111cba29408496bd8a44f
Ian Denhardt <ian@zenhack.net>
2021-01-07 20:14:45 -0500

    grammar

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -74,7 +74,7 @@ wait_for mysql $mysql_socket
 if [ ! -e /var/.db-created ]; then
     mysql --user "$MYSQL_USER" -e "CREATE DATABASE $MYSQL_DATABASE"
     mysql --user "$MYSQL_USER" --database "$MYSQL_DATABASE" < /opt/app/schema/ttrss_schema_mysql.sql
-    # Delete the TTRSS project's feeds from the db; otherwise on start up the user will
+    # Delete the TTRSS project's feeds from the db; otherwise the user will
     # get a bunch of powerbox requests immediately on startup, which is not great UX:
     mysql --user "$MYSQL_USER" --database "$MYSQL_DATABASE" -e "DELETE FROM ttrss_feeds"
     touch /var/.db-created
