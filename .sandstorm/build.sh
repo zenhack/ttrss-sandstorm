6cdad054973790f1b149c0b9c8f217ae688b6f18
Ian Denhardt <ian@zenhack.net>
2021-12-31 22:51:07 -0500

    Bin the version of nix that we use.
    Apparently >=2.4 doesn't work with the old nixpkgs pin we have.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -2,15 +2,28 @@
 
 set -euo pipefail
 
+
 # Install mysql 5.5 from nix, for upgrading old grains:
 if [ ! -d /nix/store ]; then
-	sh <(curl -L https://nixos.org/nix/install) --no-daemon
+	# Install a version of nix that is old enough to work
+	# with the even-older version of nixpkgs that shipped
+	# mysql 5.5. The logic was worked out by reading the
+	# install script normally piped into bash; we can't just
+	# do that because it will give us the newest version.
+	nix_version=2.3
+	nix_url="https://releases.nixos.org/nix/nix-${nix_version}/nix-${nix_version}-x86_64-linux.tar.xz"
+	curl -L "$nix_url" -o nix.tar.xz
+	mkdir nix-unpack
+	cd nix-unpack
+	tar -xf ../nix.tar.xz
+	./*/install --no-daemon
 fi
 . /home/vagrant/.nix-profile/etc/profile.d/nix.sh
 cd /home/vagrant
 if [ ! -d nixpkgs ]; then
 	git clone https://github.com/nixos/nixpkgs
 	cd nixpkgs
+	# Last version of nixpkgs that shipped mysql 5.5
 	git checkout 880bc93fc0ad44ea5b973e532c338afeb70d2a71
 fi
 sudo ln -sf \
